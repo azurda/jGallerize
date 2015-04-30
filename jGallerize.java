e642513e746beab5fe320a63dbdd712ab36c56b5
ineedblood <aztec_eu@live.com>
2015-04-30 12:26:13 +0200

    Imagecode generator
    jGallery now creates a HTML file with the files found on the folder.

--- jGallerize.java
+++ jGallerize.java
@@ -13,16 +13,43 @@ import java.nio.file.Path;
 public class Gallerize{
 	public static void main(String[] args)
 	{
-		String s = "<html lang=en><head><meta charset=utf-8><title>jGallerize template</title><meta name=description content=HTML jGallerize><meta name=author content=Nando><link rel=stylesheet href=css/styles.css?v=1.0><!--[if lt IE 9]>  <script src=http://html5shiv.googlecode.com/svn/trunk/html5.js></script>  <![endif]--></head><body>  <script src=js/scripts.js></script></body></html>";
-			byte data[] = s.getBytes();
-		Path p = Paths.get("./logfile.html");
+	// Get the template string
+		String s = "<html lang=en><head><meta charset=utf-8><title>jGallerize template</title><meta name=description content=HTML jGallerize><meta name=author content=Nando><link rel=stylesheet href=css/styles.css?v=1.0><!--[if lt IE 9]>  <script src=http://html5shiv.googlecode.com/svn/trunk/html5.js></script>  <![endif]--></head><body> $IMGCODE$ <script src=js/scripts.js></script></body></html>";
+		
+		// Create file
+		Path p = Paths.get("./gallery.html");
+		
+		final String dir = System.getProperty("user.dir");
+		File folder = new File(dir);
+		File[] listOfFiles = folder.listFiles();
+		
+		int i = 0;
+		String finalcode = null;
+		for(File file : listOfFiles)
+		{
+			if(file.isFile())
+			{
+				final String code = "<img src=$FILENAME$ alt=$FILENAME$ height=42 width=100>";
+				String codeToReplace = code;
+				String modStr = codeToReplace.replace("$FILENAME$", file.getName());
+				finalcode += modStr;
+				++i;
+				// create a new line each 10 files
+				if(i % 10 == 0)
+				{
+					finalcode += "<br>";
+				}
+			}
+		}
+		// using replacement tag
+		String FINAL = s.replace("$IMGCODE$",finalcode);
+		
+		byte data[] = FINAL.getBytes();
 		try(OutputStream out = new BufferedOutputStream(Files.newOutputStream(p, CREATE, APPEND)))
 		{
 			out.write(data, 0, data.length);
 		}
 		catch (IOException x) {
 			System.err.println(x);
-		}
-
 		}
 	}
